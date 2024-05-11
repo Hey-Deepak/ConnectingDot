0119b0688f991ba501e74aebcbdf47b84591dde6
Hey-Deepak <choudharydeepak990@gmail.com>
2024-05-11 06:05:07 +0530

    Splash Screen added

--- app/src/main/java/com/ts/connectingdot/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/ts/connectingdot/feature/splash/SplashScreen.kt
@@ -6,17 +6,23 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.ts.connectingdot.R
 import com.ts.connectingdot.ui.theme.Gradient
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navController: NavHostController) {
+
+    LaunchedEffect(key1 = Unit) {
+
+    }
 
     Box(
         modifier = Modifier
